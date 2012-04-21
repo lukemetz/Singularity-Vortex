51eb884b0939276468e602374eced8116dc9fe4b
Brendan Ritter <brendan@brendanritter.(none)>
2012-04-21 13:37:54 -0400

    modularized loadImage.py by putting load and save in functions

--- loadImage.py
+++ loadImage.py
@@ -1,29 +1,23 @@
-import scipy
-import scipy.ndimage.interpolation
-from PIL import Image
-import numpy
+def load_image(imagefilepath):
+	import scipy
+	import scipy.ndimage.interpolation
+	from PIL import Image
+	import numpy
 
-import pdb
+	import pdb
 
-im = Image.open("bunny.jpg")
-array = scipy.misc.fromimage(im)
+	im = Image.open(imagefilepath)
+	array = scipy.misc.fromimage(im)
 
-rarray = array[:,:,0]
-garray = array[:,:,1]
-barray = array[:,:,2]
+	rarray = array[:,:,0]
+	garray = array[:,:,1]
+	barray = array[:,:,2]
+	return [rarray,garray,barray]
 
+def save_image(red,green,blue):
+	#rmatrix = numpy.matrix(rarray)
+	#gmatrix= numpy.matrix(garray)
+	#bmatrix = numpy.matrix(barray)
 
-#rmatrix = numpy.matrix(rarray)
-#gmatrix= numpy.matrix(garray)
-#bmatrix = numpy.matrix(barray)
-
-array = numpy.array([rarray, garray, barray])
-
-#print identity
-#pdb.set_trace()
-
-
-scipy.misc.imsave('bunnyout2.png', array)
-
-
-#print array
+	array = numpy.array([rarray, garray, barray])
+	scipy.misc.imsave('bunnyout2.png', array)
