24c6c73b632a8b284cee91c8feead194a6344081
Nassim Chloe Eghtebas <nassim.eghtebas@students.olin.edu>
2012-04-23 14:02:18 -0400

    Deleted Hello.py and added __init__.py

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from load_image import*
+# input: filepath
+from save_image import*
+# input: red, green, blue
+from applyTransformationMatrix import*
+#array, transform, newSize=None
+from flip_Image import*
+# inpus: filepath  and d where d==1 is vertical flip and d==2 is horizontal flip
+from color_balance import* 
+#imagefilepath,channel,num
+
+
